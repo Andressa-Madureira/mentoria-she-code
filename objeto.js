154fb79fbcefe9c7a8be7810a16cf93c1204b1c4
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-11 21:25:10 -0300

    first commit

---
+++ objeto.js
@@ -0,0 +1,9 @@
+let livro = {
+    titulo: "Lógica de Programação e Algoritmos com Javascript",
+    autor: "Edécio Fernando Iepsen",
+    ano: 2025
+}
+
+livro.ano = 2022
+
+console.log(`Olá! Você adquiriu o livro ${livro.titulo}. O autor desse livro é ${livro.autor}. O ano de lançamento desse livro foi em ${livro.ano}`)
---
+++ script.js
@@ -0,0 +1,16 @@
+let nome = window.prompt("Qual é o seu nome?")
+let age = window.prompt("E qual é a sua idade?")
+window.alert(`Olá! Verifiquei que o seu nome é ${nome} e você tem ${age}`) // utilizando o template string transformar a idade em string
+
+
+let pizza = window.prompt("Você gosta de pizza?")
+
+if(pizza.toLowerCase() === 'sim'){
+     window.alert("Ah, você gosta de pizza")
+}else{
+    window.alert("Você não gosta de pizza.")
+}
+
+
+
+
